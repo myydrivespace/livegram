39b0bace5e3387f3fa56f228e74296716594b22d
myydrivespace <160312692+myydrivespace@users.noreply.github.com>
2025-01-19 02:13:17 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Pyrogram setup
+API_ID = "9146586"
+API_HASH = "26a687491f2668372b88226190860a6d"
+BOT_TOKEN = "5092882387:AAEatxLFdEj-AJuTYncampkjoxX_SP_3Zt8"
+GROUP_CHAT_ID = -1002414868102
+ADMIN_ID = 378224477
+
+MONGO_URI = 'mongodb+srv://balckmarket:balckmarket@balckmarket.vgd9k.mongodb.net/?retryWrites=true&w=majority&appName=balckmarket'
